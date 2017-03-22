63b5c5ec09b1d2566cc902307dc0f3ac3585d003
Daniel Garnier-Moiroux <git@garnier.wf>
2017-03-22 22:05:42 +0100

    + Loading config from file : tests (not passing) and first implementation

--- config.go
+++ config.go
@@ -1,21 +1,34 @@
 package main
 
-import "time"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"time"
+)
 
 // Resource represents an Url to be watched, and all the necessary parameters
 type Config struct {
-	Resources []Resource `json:"resources"`
+	Resources []struct {
+		Url               string        `json:"url"`               // the Url to watch
+		IntervalInSeconds time.Duration `json:"intervalInSeconds"` // the interval at which to poll the resource. Defaults to 60s.
+		TimeoutInSeconds  time.Duration `json:"timeoutInseconds"`  // the timeout in seconds. Defaults to 2s.
+		NumberOfTries     int           `json:"numberOfTries"`     // number of attempts at getting the resource. Defaults to 10.
+		FailureThreshold  int           `json:"failureThreshold"`  // the number of failures within the tries that would raise an alarm. Defaults to 5.
+	} `json:"resources"`
 }
 
-type Resource struct {
-	Url               string        `json:"url"`               // the Url to watch
-	IntervalInSeconds time.Duration `json:"intervalInSeconds"` // the interval at which to poll the resource. Defaults to 60s.
-	TimeoutInSeconds  time.Duration `json:"timeoutInseconds"`  // the timeout in seconds. Defaults to 2s.
-	NumberOfTries     int           `json:"numberOfTries"`     // number of attempts at getting the resource. Defaults to 10.
-	FailureThreshold  int           `json:"failureThreshold"`  // the number of failures within the tries that would raise an alarm. Defaults to 5.
-}
+// LoadConfig loads a config from a JSON file
+func LoadConfig(path string) (*Config, error) {
+	file, err := ioutil.ReadFile(path)
 
-// LoadConfig loads all resources from a JSON file
-func LoadConfig(path string) Config {
-	return Config{}
+	if err != nil {
+		return nil, err
+	}
+
+	ret := Config{}
+	if json.Unmarshal(file, ret) != nil {
+		return nil, err
+	}
+
+	return &ret, nil
 }
--- config_test.go
+++ config_test.go
@@ -15,19 +15,33 @@ var _ = Describe("Config", func() {
 			defer os.Remove(tempfile.Name())
 			tempfile.Close()
 
-			Expect(tempfile.Name()).ToNot(BeNil())
+			config, error := LoadConfig(tempfile.Name())
+
+			Expect(error).To(BeNil())
+			Expect(config).ToNot(BeNil())
 		})
 	})
 
 	Context("When loading an invalid config from a file", func() {
 		It("Should throw an error", func() {
+			tempfile, _ := ioutil.TempFile("", "config.json")
+			defer os.Remove(tempfile.Name())
+			tempfile.WriteString("hello i am invalid !")
+			tempfile.Close()
 
+			config, error := LoadConfig(tempfile.Name())
+
+			Expect(error).ToNot(BeNil())
+			Expect(config).To(BeNil())
 		})
 	})
 
 	Context("When trying to load a non-existing file", func() {
 		It("Should throw an error", func() {
+			config, error := LoadConfig("i_am_not_a_file.json")
 
+			Expect(error).ToNot(BeNil())
+			Expect(config).To(BeNil())
 		})
 	})
 })

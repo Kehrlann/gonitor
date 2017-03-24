571c1943ce73d21e74982d88a455f265850c62c0
Daniel Garnier-Moiroux <git@garnier.wf>
2017-03-24 23:23:02 +0100

    Load config from json in main (but do nothing with it)

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"log"
+	"time"
+)
 
 func main() {
+	config, err := LoadConfig("./config.json")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// TODO : use config to start Goroutines
+	fmt.Println(config)
+
 	messages := make(chan *StateChangeMessage)
 	go Run("https://ratp.garnier.wf", 2*time.Second, messages)
 	go Run("http://www.google.com", 2*time.Second, messages)

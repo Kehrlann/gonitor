76fa0857d373566bb6f7d584b230e2785a039b94
Daniel Garnier-Moiroux <git@garnier.wf>
2017-03-24 19:57:29 +0100

    + Bugfix config loading

--- config.go
+++ config.go
@@ -3,17 +3,16 @@ package main
 import (
 	"encoding/json"
 	"io/ioutil"
-	"time"
 )
 
 // Resource represents an Url to be watched, and all the necessary parameters
 type Config struct {
 	Resources []struct {
-		Url               string        `json:"url"`               // the Url to watch
-		IntervalInSeconds time.Duration `json:"intervalInSeconds"` // the interval at which to poll the resource. Defaults to 60s.
-		TimeoutInSeconds  time.Duration `json:"timeoutInSeconds"`  // the timeout in seconds. Defaults to 2s.
-		NumberOfTries     int           `json:"numberOfTries"`     // number of attempts at getting the resource. Defaults to 10.
-		FailureThreshold  int           `json:"failureThreshold"`  // the number of failures within the tries that would raise an alarm. Defaults to 5.
+		Url               string `json:"url"`               // the Url to watch
+		IntervalInSeconds int    `json:"intervalInSeconds"` // the interval at which to poll the resource. Defaults to 60s.
+		TimeoutInSeconds  int    `json:"timeoutInSeconds"`  // the timeout in seconds. Defaults to 2s.
+		NumberOfTries     int    `json:"numberOfTries"`     // number of attempts at getting the resource. Defaults to 10.
+		FailureThreshold  int    `json:"failureThreshold"`  // the number of failures within the tries that would raise an alarm. Defaults to 5.
 	} `json:"resources"`
 }
 
@@ -25,10 +24,10 @@ func LoadConfig(path string) (*Config, error) {
 		return nil, err
 	}
 
-	ret := Config{}
+	ret := &Config{}
 	if err := json.Unmarshal(file, ret); err != nil {
 		return nil, err
 	}
 
-	return &ret, nil
+	return ret, nil
 }

a80ac05ffd267f9e3fa34502ca32640b15002e58
Daniel Garnier-Moiroux <git@garnier.wf>
2017-03-26 22:18:21 +0200

    Refactor : one client per site, not one client per iteration per site

--- http.go
+++ http.go
@@ -1,31 +1,25 @@
 package main
 
 import (
-	"net"
 	"net/http"
 	"time"
 )
 
-var timeout = 1 * time.Second
-
 // Run takes a resource and polls the given HTTP url for errors , and emits failure / recovery messages accordingly
 func Run(resource Resource, messages chan<- *StateChangeMessage) {
 	responseCodes := make(chan int)
+	client := &http.Client{
+		Timeout: time.Duration(resource.TimeoutInSeconds) * time.Second,
+	}
+
 	go Analyze(resource, responseCodes, messages)
+
 	for range time.Tick(time.Duration(resource.IntervalInSeconds) * time.Second) {
-		responseCodes <- fetch(resource.Url, resource.TimeoutInSeconds)
+		responseCodes <- fetch(client, resource.Url)
 	}
 }
 
-func fetch(url string, timeOutInSeconds int) int {
-	netTransport := &http.Transport{
-		Dial:                (&net.Dialer{Timeout: time.Duration(timeOutInSeconds) * time.Second}).Dial,
-		TLSHandshakeTimeout: time.Duration(timeOutInSeconds) * time.Second,
-	}
-	client := &http.Client{
-		Timeout:   time.Duration(timeOutInSeconds) * time.Second,
-		Transport: netTransport,
-	}
+func fetch(client *http.Client, url string) int {
 
 	resp, err := client.Get(url)
 	if err != nil {

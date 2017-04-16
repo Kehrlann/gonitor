9a0de2557c22d85a8c8313fcf41e06cb8333b72c
Daniel Garnier-Moiroux <git@garnier.wf>
2017-04-16 19:35:13 +0200

    Add http test

---
+++ http_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"gopkg.in/jarcoal/httpmock.v1"
+	"net/http"
+)
+
+var _ = Describe("Http", func() {
+
+	Describe("Fetch", func() {
+
+		Context("When fetching from a website", func() {
+
+			It("Should get the correct code", func() {
+
+				hasRequestBeenMade := false
+				httpmock.Activate()
+				defer httpmock.DeactivateAndReset()
+				httpmock.RegisterResponder(
+					"GET",
+					"http://example.com",
+					func(req *http.Request) (*http.Response, error) {
+						hasRequestBeenMade = true
+						return httpmock.NewStringResponse(200, `Woo-hoo`), nil
+					})
+
+				status_code := fetch(&http.Client{}, "http://example.com")
+
+				Expect(status_code).To(Equal(200))
+				Expect(hasRequestBeenMade).To(BeTrue())
+			})
+		})
+	})
+
+})

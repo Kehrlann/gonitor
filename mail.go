210c0ec9381fa0d433ba53deaa344f831b758b62
Daniel Garnier <git@garnier.wf>
2017-04-18 22:10:02 +0200

    Ignore linux built binary

--- mail.go
+++ mail.go
@@ -4,18 +4,18 @@ import (
 	"gopkg.in/gomail.v2"
 )
 
-func SendMail(smtp *Smtp, message *StateChangeMessage) {
-	// TODO : HERE
-	// TODO : HERE
+func SendMail(smtp *Smtp, message StateChangeMessage) {
+	// prepare
 	m := gomail.NewMessage()
 	m.SetHeader("From", smtp.From)
 	m.SetHeader("To", smtp.To...)
-	m.SetHeader("Subject", "Gonitor : Error for ")
-	m.SetBody("text/html", "Hello <b>Bob</b> and <i>Cora</i>!")
+	m.SetHeader("Subject", message.MailSubject())
+	m.SetBody("text/html", message.MailBody())
 
+	// connect
 	d := gomail.NewDialer(smtp.Host, smtp.Port, smtp.Username, smtp.Password)
 
-	// Send the email to Bob, Cora and Dan.
+	// send & auto-close
 	if err := d.DialAndSend(m); err != nil {
 		// TODO : LOG ERROR
 	}

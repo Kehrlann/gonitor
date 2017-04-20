bcc7cf8216faf445babdcb8c3b0cf0fd423a18e8
Daniel Garnier-Moiroux <git@garnier.wf>
2017-04-20 21:25:00 +0200

    Put the "config.LoadConfig" tests in their own describe block

--- config_test.go
+++ config_test.go
@@ -7,12 +7,13 @@ import (
 	"os"
 )
 
-var _ = Describe("Config", func() {
+var _ = Describe("Config : ", func() {
 
-	Context("When loading a valid config from a file", func() {
-		It("Should return the proper resources", func() {
-			// Arrange
-			tempFile := createConfigFile(`{
+	Describe("LoadConfig : ", func() {
+		Context("When loading a valid config from a file", func() {
+			It("Should return the proper resources", func() {
+				// Arrange
+				tempFile := createConfigFile(`{
 				"resources" : 	[
 									{
 										"url" : 				"http://www.example.com",
@@ -30,27 +31,27 @@ var _ = Describe("Config", func() {
 									}
 								]
 			}`)
-			defer os.Remove(tempFile.Name())
+				defer os.Remove(tempFile.Name())
 
-			// Act
-			config, err := LoadConfig(tempFile.Name())
+				// Act
+				config, err := LoadConfig(tempFile.Name())
 
-			// Assert
-			Expect(err).To(BeNil())
-			Expect(config).ToNot(BeNil())
-			Expect(len(config.Resources)).To(Equal(2))
+				// Assert
+				Expect(err).To(BeNil())
+				Expect(config).ToNot(BeNil())
+				Expect(len(config.Resources)).To(Equal(2))
 
-			first := config.Resources[0]
-			resource := Resource{"http://www.example.com", 60, 2, 10, 3}
-			Expect(first).To(Equal(resource))
+				first := config.Resources[0]
+				resource := Resource{"http://www.example.com", 60, 2, 10, 3}
+				Expect(first).To(Equal(resource))
 
-			second := config.Resources[1]
-			resource = Resource{"http://www.example.test", 120, 10, 10, 10}
-			Expect(second).To(Equal(resource))
-		})
+				second := config.Resources[1]
+				resource = Resource{"http://www.example.test", 120, 10, 10, 10}
+				Expect(second).To(Equal(resource))
+			})
 
-		It("Should load the SMTP config when it exists", func() {
-			tempFile := createConfigFile(`{
+			It("Should load the SMTP config when it exists", func() {
+				tempFile := createConfigFile(`{
 				"smtp" :	{
 								"host" 			: "smtp.example.com",
 								"port"			: 25,
@@ -61,47 +62,48 @@ var _ = Describe("Config", func() {
 								"to" 			: ["recipient@example.com", "admin@example.com"]
 							}
 			}`)
-			defer os.Remove(tempFile.Name())
+				defer os.Remove(tempFile.Name())
 
-			// Act
-			config, err := LoadConfig(tempFile.Name())
+				// Act
+				config, err := LoadConfig(tempFile.Name())
 
-			// Assert
-			Expect(err).To(BeNil())
-			Expect(config).ToNot(BeNil())
+				// Assert
+				Expect(err).To(BeNil())
+				Expect(config).ToNot(BeNil())
 
-			smtp := config.Smtp
-			Expect(smtp.Host).To(Equal("smtp.example.com"))
-			Expect(smtp.Port).To(Equal(25))
-			Expect(smtp.FromAddress).To(Equal("address@example.com"))
-			Expect(smtp.FromName).To(Equal("Mr Example"))
-			Expect(smtp.Username).To(Equal("user"))
-			Expect(smtp.Password).To(Equal("password123"))
-			Expect(smtp.To).To(ConsistOf("recipient@example.com", "admin@example.com"))
+				smtp := config.Smtp
+				Expect(smtp.Host).To(Equal("smtp.example.com"))
+				Expect(smtp.Port).To(Equal(25))
+				Expect(smtp.FromAddress).To(Equal("address@example.com"))
+				Expect(smtp.FromName).To(Equal("Mr Example"))
+				Expect(smtp.Username).To(Equal("user"))
+				Expect(smtp.Password).To(Equal("password123"))
+				Expect(smtp.To).To(ConsistOf("recipient@example.com", "admin@example.com"))
 
+			})
 		})
-	})
 
-	Context("When loading an invalid config from a file", func() {
-		It("Should throw an error", func() {
-			tempfile, _ := ioutil.TempFile("", "gonitor.config.json")
-			defer os.Remove(tempfile.Name())
-			tempfile.WriteString("hello i am invalid !")
-			tempfile.Close()
+		Context("When loading an invalid config from a file", func() {
+			It("Should throw an error", func() {
+				tempfile, _ := ioutil.TempFile("", "gonitor.config.json")
+				defer os.Remove(tempfile.Name())
+				tempfile.WriteString("hello i am invalid !")
+				tempfile.Close()
 
-			config, err := LoadConfig(tempfile.Name())
+				config, err := LoadConfig(tempfile.Name())
 
-			Expect(err).ToNot(BeNil())
-			Expect(config).To(BeNil())
+				Expect(err).ToNot(BeNil())
+				Expect(config).To(BeNil())
+			})
 		})
-	})
 
-	Context("When trying to load a non-existing file", func() {
-		It("Should throw an error", func() {
-			config, err := LoadConfig("i_am_not_a_file.json")
+		Context("When trying to load a non-existing file", func() {
+			It("Should throw an error", func() {
+				config, err := LoadConfig("i_am_not_a_file.json")
 
-			Expect(err).ToNot(BeNil())
-			Expect(config).To(BeNil())
+				Expect(err).ToNot(BeNil())
+				Expect(config).To(BeNil())
+			})
 		})
 	})
 })

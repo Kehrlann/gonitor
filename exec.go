5e96b149b555feb8a875ac9dd20bc3cc6b1051d0
Daniel Garnier-Moiroux <git@garnier.wf>
2017-05-04 23:00:39 +0200

    + More testing of ExecCommand

--- exec.go
+++ exec.go
@@ -4,14 +4,19 @@ import (
 	"os/exec"
 	"bytes"
 	"fmt"
+	"time"
 )
 
-func ExecCommand(globalCommand string, message *StateChangeMessage) *bytes.Buffer {
+
+// Exec command execute the command for the resource. If it does not exist, ExecuteCommand tries to run the default
+// global command passed. It returns a byte buffer, with both StdErr and StdOut in it, mostly for testing purposes.
+// Be careful ! This function waits for the command to return, so it might cause massive problems, leaks ...
+func ExecCommand(message *StateChangeMessage, defaultCommand string) *bytes.Buffer {
 	var command string
 	if message.Resource.Command != "" {
 		command = message.Resource.Command
-	} else if globalCommand != "" {
-		command = globalCommand
+	} else if defaultCommand != "" {
+		command = defaultCommand
 	} else {
 		return nil
 	}
@@ -23,9 +28,10 @@ func ExecCommand(globalCommand string, message *StateChangeMessage) *bytes.Buffe
 		recoveryOrFailure = "FAILURE"
 	}
 
-	cmd := exec.Command(command, recoveryOrFailure, message.Datetime.String(), fmt.Sprint(message.Codes), message.Resource.Url)
+	cmd := exec.Command(command, recoveryOrFailure, message.Datetime.Format(time.RFC3339), fmt.Sprint(message.Codes), message.Resource.Url)
 	var out bytes.Buffer
 	cmd.Stdout = &out
+	cmd.Stderr = &out
 	cmd.Run()
 	return &out
 }
--- exec_test.go
+++ exec_test.go
@@ -8,22 +8,37 @@ import (
 
 var _ = Describe("exec : ", func() {
 	Describe("ExecCommand : ", func() {
-		resource := Resource{"http://www.example.com", 60, 2, 10, 3, ""}
-		message := RecoveryMessage(resource, []int{200, 200, 200})
+		resourceWithoutCommand := Resource{"http://www.example.com", 60, 2, 10, 3, ""}
+
+		message := RecoveryMessage(resourceWithoutCommand, []int{200, 200, 200})
 
 		It("Not run any command if not defined", func() {
-			ret := ExecCommand("", message)
-
+			ret := ExecCommand(message, "")
 			Expect(ret).To(BeNil())
 		})
 
-		It("Should run the global command if defined", func() {
-			// TODO : fix me :)
-			// Note : don't run on windows because 'echo' is weird on that platform
-			if runtime.GOOS != "windows" {
-				ret := ExecCommand("echo", message)
-				Expect(ret.String()).To(Equal("coucou"))
-			}
-		})
+		// Note : don't run on windows because 'echo' is weird on that platform
+		if runtime.GOOS != "windows" {
+			It("Should run the global command if defined", func() {
+				ret := ExecCommand(message, "echo")
+				Expect(ret.String()).To(ContainSubstring("RECOVERY"))
+			})
+
+			It("Should run the resource command if defined", func() {
+				resourceWithCommand := Resource{"http://www.example.com", 60, 2, 10, 3, "echo"}
+				messageWithCommand := RecoveryMessage(resourceWithCommand, []int{200, 200, 200})
+
+				ret := ExecCommand(messageWithCommand, "")
+				Expect(ret.String()).To(ContainSubstring("RECOVERY"))
+			})
+
+			It("Should override the global command if both are defined", func() {
+				resourceWithCommand := Resource{"http://www.example.com", 60, 2, 10, 3, "go"}
+				messageWithCommand := RecoveryMessage(resourceWithCommand, []int{200, 200, 200})
+
+				ret := ExecCommand(messageWithCommand, "")
+				Expect(ret.String()).To(ContainSubstring("unknown subcommand"))
+			})
+		}
 	})
 })

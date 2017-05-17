7407c96309c6aa0ad99e845f17310a45140650f9
Daniel Garnier-Moiroux <git@garnier.wf>
2017-05-17 20:02:07 +0200

    Thread-safe emit/worker tests

--- emit/worker_test.go
+++ emit/worker_test.go
@@ -3,27 +3,14 @@ package emit
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"sync"
 )
-
-// since the Emit method is called asynchronously, we use a chan of messages to denoted that it has been called ;
-// instead of a simple boolean (which causes a data-race)
-type fakeEmitter struct {
-	calls chan *StateChangeMessage
-}
-
-func (emitter *fakeEmitter) Emit(m *StateChangeMessage) {
-	emitter.calls <- m
-}
-
-func newFake() *fakeEmitter{
-	return &fakeEmitter{make(chan *StateChangeMessage)}
-}
 
 var _ = Describe("Worker", func() {
 
 	Describe("emitMessages", func() {
 		It("Should Emit messages on all configured emitters", func() {
-			emitters := []emitter{newFake(), newFake()}
+			emitters := []emitter{newFakeChan(), newFakeMutex()}
 			messages := make(chan *StateChangeMessage, 1)
 			messages <- &StateChangeMessage{}
 			close(messages)
@@ -31,8 +18,41 @@ var _ = Describe("Worker", func() {
 			// emitMessages is blocking, so should be run asynchronously
 			emitMessages(emitters, messages)
 
-			Eventually(emitters[0].(*fakeEmitter).calls).Should(Receive())
-			Eventually(emitters[1].(*fakeEmitter).calls).Should(Receive())
+			Eventually(emitters[0].(*fakeChanEmitter).calls).Should(Receive())
+			Eventually(emitters[1].(*fakeMutexEmitter).HasBeenCalled).Should(BeTrue())
 		})
 	})
 })
+
+type fakeChanEmitter struct {
+	calls chan *StateChangeMessage
+}
+
+func (emitter *fakeChanEmitter) Emit(m *StateChangeMessage) {
+	emitter.calls <- m
+}
+
+func newFakeChan() *fakeChanEmitter {
+	return &fakeChanEmitter{make(chan *StateChangeMessage)}
+}
+
+type fakeMutexEmitter struct {
+	hasBeenCalled bool
+	lock sync.Mutex
+}
+
+func (emitter *fakeMutexEmitter) Emit(m *StateChangeMessage) {
+	emitter.lock.Lock()
+	defer emitter.lock.Unlock()
+	emitter.hasBeenCalled = true
+}
+
+func (emitter *fakeMutexEmitter) HasBeenCalled() bool {
+	emitter.lock.Lock()
+	defer emitter.lock.Unlock()
+	return emitter.hasBeenCalled
+}
+
+func newFakeMutex() *fakeMutexEmitter {
+	return &fakeMutexEmitter{false, sync.Mutex{}}
+}
